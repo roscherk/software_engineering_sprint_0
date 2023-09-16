c8d88bcd9e7e0a24e123b6c424f3854cc402dd34
roscherk <70962699+roscherk@users.noreply.github.com>
2023-09-16 22:33:33 +0300

    feat: rework ui

--- lib/calc_button.dart
+++ lib/calc_button.dart
@@ -1,29 +1,42 @@
 import 'package:flutter/material.dart';
 
 class CalcButton extends StatelessWidget {
+  final String buttonText;
+  final Size size;
+  final onPressed;
   final Color borderColor;
   final Color textColor;
-  final String buttonText;
-  const CalcButton({super.key, required this.borderColor, required this.textColor, required this.buttonText});
+
+  const CalcButton(
+      {super.key,
+        required this.buttonText,
+        required this.size,
+        required this.onPressed,
+        this.borderColor = Colors.white,
+        this.textColor = Colors.white});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(5.0),
-        child: OutlinedButton(
-          style: OutlinedButton.styleFrom(
-            // shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0))),
-            shape: const ContinuousRectangleBorder(),
-            side: BorderSide(color: borderColor)
-          ),
-          onPressed: () {
-              debugPrint('$buttonText pressed');
-          },
-          child: Text(buttonText, style: TextStyle(color: textColor),)
-    ));
-  }
-
-  void _processTap() {
-
+    return SizedBox(
+      width: size.width,
+      height: size.height,
+      child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                    // shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                    shape: const ContinuousRectangleBorder(),
+                    side: BorderSide(color: borderColor)),
+                onPressed: onPressed,
+                child: Text(
+                  buttonText,
+                  style: TextStyle(color: textColor),
+                )
+            ),
+          )
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class Calculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Calculator',
-      theme: ThemeData(
+      theme: ThemeData(  // это бесполезная штука, которая нигде не используется
           colorScheme: const ColorScheme(
               brightness: Brightness.dark,
               primary: Colors.white,
@@ -26,9 +26,7 @@ class Calculator extends StatelessWidget {
               onBackground: Colors.white,
               surface: Colors.white60,
               onSurface: Colors.white),
-          primaryColor: Colors.black,
-          textTheme: GoogleFonts.pressStart2pTextTheme()
-          ),
+          textTheme: GoogleFonts.pressStart2pTextTheme()),
       home: const CalculatorHomePage(),
     );
   }
@@ -42,61 +40,114 @@ class CalculatorHomePage extends StatefulWidget {
 }
 
 class _CalculatorHomePageState extends State<CalculatorHomePage> {
-  final String currentExpression = '0';
-  // void _incrementCounter() {
-  //   setState(() {
-  //     // This call to setState tells the Flutter framework that something has
-  //     // changed in this State, which causes it to rerun the build method below
-  //     // so that the display can reflect the updated values. If we changed
-  //     // _counter without calling setState(), then the build method would not be
-  //     // called again, and so nothing would appear to happen.
-  //   });
-  // }
+  String currentExpression = '';
+
+  void _handleButtonTap(String buttonText) {
+    const List<String> operations = ['C', 'DEL', '±', '÷', '×', '-', '+', '='];
+    // String lastChar = currentExpression.substring(0, currentExpression.length - 1);
+    if (operations.contains(buttonText)) {
+      if (buttonText == 'C') {
+        setState(() {
+          currentExpression = '';
+        });
+        return;
+      } else if (buttonText == 'DEL') {
+        setState(() {
+          currentExpression = currentExpression.substring(0, currentExpression.length - 1);
+        });
+        return;
+      } else if (buttonText == '±') {
+        //TODO
+        return;
+      } else if (buttonText == '=') {
+        //TODO
+        return;
+      } else if (operations.contains(currentExpression.substring(currentExpression.length - 1))) {
+        setState(() {
+          currentExpression = currentExpression.substring(0, currentExpression.length - 1) + buttonText;
+        });
+        return;
+      }
+    }
+    setState(() {
+      currentExpression += buttonText;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    List<String> buttons = [
-      'C', 'DEL', '±', '÷',
-      '7', '8', '9', '×',
-      '4', '5', '6', '-',
-      '1', '2', '3', '+',
-      '0', '(', ')', '='
+    const List<List<String>> buttons = [
+      ['C', 'DEL', '±', '÷',],
+      ['7', '8', '9', '×',],
+      ['4', '5', '6', '-',],
+      ['1', '2', '3', '+',],
+      ['0', '(', ')', '=']
     ];
-    debugPrint('viewPadding: ${MediaQuery.of(context).viewPadding.top}');
-    debugPrint('viewInsets: ${MediaQuery.of(context).viewInsets.top}');
+    // debugPrint('viewPadding.top: ${MediaQuery.of(context).viewPadding.top}');
+    // debugPrint('viewInsets.bottom: ${MediaQuery.of(context).padding.bottom}');
     // var style = ElevatedButton.styleFrom(
     //   shape: const CircleBorder(),
     //   backgroundColor: Colors.transparent,
     //   textStyle: const TextStyle(fontSize: 60),
     // );
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          Expanded(
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(8, MediaQuery.of(context).viewPadding.top + 5, 8, 0),
-                child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: Text(currentExpression, style: TextStyle(color: Colors.white),),
-                )
-              )
-          ),
-          Expanded(
-            flex: 2,
-            child: GridView.builder(
-                itemCount: buttons.length,
-                physics: const NeverScrollableScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 4),
-                itemBuilder: (BuildContext context, int index) {
-                  return CalcButton(
-                      borderColor: Colors.white,
-                      textColor: Colors.white,
-                      buttonText: buttons[index]);
-                }),
-          ),
-        ],
+      // backgroundColor: Colors.black,
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constrains) {
+          var availableHeight = constrains.maxHeight - MediaQuery.of(context).viewPadding.top - MediaQuery.of(context).viewPadding.bottom;
+          var textBoxSize = availableHeight / 3;
+          var keyboardSize = 2 * availableHeight / 3;
+          var buttonSize = Size(constrains.maxWidth / buttons.first.length, keyboardSize / buttons.length);
+          // debugPrint(availableHeight.toString());
+          // debugPrint(buttonSize.toString());
+          return Padding(
+            padding: EdgeInsets.only(top: MediaQuery.of(context).viewPadding.top),
+            child: Column(
+              children: [
+                SizedBox(
+                  width: constrains.maxWidth,
+                  height: textBoxSize,
+                  child: Padding(
+                      padding: const EdgeInsets.all(8),
+                        child: Align(
+                          alignment: Alignment.bottomRight,
+                          child: Text(
+                            currentExpression,
+                            style: Theme.of(context)
+                                .textTheme
+                                .displaySmall!
+                                .copyWith(color: Colors.white),
+                          ),
+                        )
+                    )
+                ),
+                SizedBox(
+                    width: constrains.maxWidth,
+                    height: keyboardSize,
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Table(
+                        children: List<TableRow>.generate(
+                            buttons.length,
+                                (row) =>
+                                TableRow(
+                                    children: List<CalcButton>.generate(
+                                        buttons.first.length,
+                                            (index) =>
+                                            CalcButton(
+                                                buttonText: buttons[row][index],
+                                                size: buttonSize,
+                                                onPressed: () => _handleButtonTap(buttons[row][index]))
+                                    )
+                                )
+                        ),
+                      ),
+                    )
+                ),
+              ],
+            ),
+          );
+        }
       ),
     );
   }
