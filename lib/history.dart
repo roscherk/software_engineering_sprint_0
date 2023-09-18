f2374c587d32c8fdcaf949ffa56889f22933cf3b
DmGvozd <mihailzubienko@gmail.com>
2023-09-18 19:18:27 +0300

    feat: History page was added

--- lib/history.dart
+++ lib/history.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 class HistoryPage extends StatelessWidget {
   const HistoryPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold (
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: EdgeInsets.only(top: MediaQuery.of(context).viewPadding.top) + const EdgeInsets.all(8.0),
+        child: Align(
+          alignment: Alignment.topLeft,
+          child: TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            style: TextButton.styleFrom(
+              shape: const BeveledRectangleBorder(),
+              side: const BorderSide(color: Colors.transparent),
+            ),
+            child: Text('BACK', style: Theme.of(context).textTheme.headlineSmall)),
+        ),
+      )
+      // Textbox for expression_list
+              // Padding()
+    );           
   }
 }
